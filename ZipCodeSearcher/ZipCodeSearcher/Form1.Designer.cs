0cfa5945c90c3b28471302892826006ab6f66e7a
JunhyungSong <32870787+JunhyungSong@users.noreply.github.com>
2017-11-05 19:36:54 +0900

    Add files via upload

---
+++ ZipCodeSearcher/ZipCodeSearcher/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ZipCodeSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.City = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ZipCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.State = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += TextBox1_TextChanged;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "City Name";
+            // 
+            // listView1
+            // 
+            this.City.Text = "City";
+            this.ZipCode.Text = "ZipCode";
+            this.State.Text = "State";
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.City,
+            this.ZipCode,
+            this.State});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Location = new System.Drawing.Point(39, 93);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(408, 270);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 375);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader City;
+        private System.Windows.Forms.ColumnHeader ZipCode;
+        private System.Windows.Forms.ColumnHeader State;
+    }
+}
+
---
+++ ZipCodeSearcher/ZipCodeSearcher/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.XPath;
+
+namespace ZipCodeSearcher
+{
+    class ZipCodeInfo
+    {
+        private uint zipCode;
+        private string cityName;
+        private string state;
+
+        public uint ZipCode { get => zipCode; }
+        public string CityName { get => cityName; }
+        public string State { get => state; }
+
+        public ZipCodeInfo(UInt32 zipCode, String cityName, String state)
+        {
+            this.zipCode = zipCode;
+            this.cityName = cityName;
+            this.state = state;
+        }
+    }
+
+    class TrieNode<T>
+    {
+        private T value;
+        private Dictionary<char, TrieNode<T>> children;
+
+        public T Value { get => value; }
+        internal Dictionary<char, TrieNode<T>> Children { get => children; }
+
+        public TrieNode(T value)
+        {
+            this.value = value;
+            this.children = new Dictionary<char, TrieNode<T>>();
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private TrieNode<List<ZipCodeInfo>> header;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.header = ParseXMLDatabaseToTrie(@"zipcode.xml");
+        }
+
+        private TrieNode<List<ZipCodeInfo>> ParseXMLDatabaseToTrie(string xmlFilePath)
+        {
+            TrieNode<List<ZipCodeInfo>> header = new TrieNode<List<ZipCodeInfo>>(null);
+
+            XPathDocument xmlDocument = new XPathDocument(xmlFilePath);
+            XPathNavigator xmlNavigator = xmlDocument.CreateNavigator();
+
+            xmlNavigator.MoveToRoot();
+
+            if (xmlNavigator.HasChildren)
+            {
+                xmlNavigator.MoveToFirstChild();
+
+                if (xmlNavigator.HasChildren)
+                {
+                    xmlNavigator.MoveToFirstChild();
+
+                    do
+                    {
+                        xmlNavigator.MoveToFirstChild();
+                        uint zipCode = uint.Parse(xmlNavigator.Value);
+
+                        xmlNavigator.MoveToNext();
+                        string cityName = xmlNavigator.Value;
+
+                        xmlNavigator.MoveToNext();
+                        string state = xmlNavigator.Value;
+
+                        ZipCodeInfo zipCodeInfo = new ZipCodeInfo(zipCode, cityName, state);
+
+                        TrieNode<List<ZipCodeInfo>> currentNode = header;
+                        for (int i = 0; i < cityName.Length; i++)
+                        {
+                            TrieNode<List<ZipCodeInfo>> findNode = null;
+                            if (!currentNode.Children.TryGetValue(cityName[i], out findNode))
+                            {
+                                if (i == cityName.Length - 1)
+                                {
+                                    currentNode.Children.Add(cityName[i], new TrieNode<List<ZipCodeInfo>>(new List<ZipCodeInfo> { zipCodeInfo }));
+                                }
+                                else
+                                {
+                                    findNode = new TrieNode<List<ZipCodeInfo>>(new List<ZipCodeInfo>());
+                                    currentNode.Children.Add(cityName[i], findNode);
+                                    currentNode = findNode;
+                                }
+                            }
+                            else
+                            {
+                                if (i == cityName.Length - 1)
+                                {
+                                    findNode.Value.Add(zipCodeInfo);
+                                }
+                                else
+                                {
+                                    currentNode = findNode;
+                                }
+                            }                            
+                        }
+
+                        xmlNavigator.MoveToParent();
+                    }
+                    while (xmlNavigator.MoveToNext());
+                }                
+            }
+
+            return header;
+        }
+
+        private void TextBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            if (sender.GetType() == typeof(TextBox))
+            {
+                this.listView1.Items.Clear();
+
+                string inputText = ((TextBox)sender).Text.ToUpper();
+                TrieNode<List<ZipCodeInfo>> currentNode = this.header;
+
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    TrieNode<List<ZipCodeInfo>> findNode = null;
+                    if (currentNode.Children.TryGetValue(inputText[i], out findNode))
+                    {
+                        currentNode = findNode;
+                    }
+                    else
+                    {
+                        currentNode = null;
+                        break;
+                    }
+                }
+
+                if (currentNode != null && currentNode != this.header)
+                {
+                    List<ListViewItem> items = new List<ListViewItem>();
+                    TraversalForNode(currentNode, ref items);
+                    ListViewItem[] arrItems = items.ToArray();
+                    this.listView1.BeginUpdate();
+                    this.listView1.Items.AddRange(arrItems);
+                    this.listView1.EndUpdate();
+                }
+            }
+        }
+
+        private void TraversalForNode(TrieNode<List<ZipCodeInfo>> node, ref List<ListViewItem> items)
+        {
+            if (node.Value != null)
+            {
+                foreach (ZipCodeInfo info in node.Value)
+                {
+                    string[] row = { info.CityName, info.ZipCode.ToString(), info.State };
+                    ListViewItem item = new ListViewItem(row);
+                    items.Add(item);
+                }
+            }            
+
+            foreach (TrieNode<List<ZipCodeInfo>> child in node.Children.Values)
+            {
+                TraversalForNode(child, ref items);
+            }
+        }
+    }
+}
